72ddc1db8ba77cba4a92f3a998ba0673474c7feb
Andrew Mason <mason@case.edu>
2015-10-16 19:48:26 -0400

    add more tokens. plus/times are annoying to get working right

--- Lex.hs
+++ Lex.hs
@@ -8,21 +8,28 @@ import Text.Regex hiding (mkRegex)
 data Token =
     TBool String |
     TInt String |
+    TOp String |
+    TIdent String |
     TErr
     deriving Show
 
 mkRegex :: String -> Regex
 mkRegex s = mkRegexWithOpts ("^" ++ s) True True
 
-boolRe = mkRegex "(true|false)"
+opRe = mkRegex ('(':'\\':'+':'|':'\\':'*':"|=|-|/|%)")
+boolRe = mkRegex "(true|false)\b"
 intRe = mkRegex "([[:digit:]]+)"  -- start with nonnegative integers for now
+identRe = mkRegex "[_a-z][_a-zA-Z0-9]*"
 
 tokenize :: String -> [Token]
 tokenize [] = []
 tokenize line@(x:xs)
     | x == ' ' || x == '\n' || x == '\t' = tokenize xs
     | otherwise = tok:tokenize rem
-    where (tok, rem) = subtokenize [(boolRe, TBool), (intRe, TInt)] line
+    where (tok, rem) = subtokenize [(opRe, TOp)
+                                   ,(boolRe, TBool)
+                                   ,(intRe, TInt)
+                                   ,(identRe, TIdent)] line
 
 subtokenize :: [(Regex, (String -> Token))] -> String -> (Token, String)
 subtokenize [] _ = (TErr, "")
