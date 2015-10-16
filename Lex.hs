97594c586b43d013fd8a5b9ee7239d11059aa69a
Andrew Mason <mason@case.edu>
2015-10-16 19:09:55 -0400

    much simpler lexing, much easier to understand
    also, ignore swapfiles

--- Lex.hs
+++ Lex.hs
@@ -1,41 +1,30 @@
 module Lex
 ( Token(..)
-{-
-, Literal(..)
-, Keyword(..)
--}
 ) where
 
 import Text.Regex hiding (mkRegex)
 
 data Token =
-    BoolToken Bool |
-    IntegerToken Int
+    TBool String |
+    TInt String |
+    TErr
     deriving Show
-
-boolRegex = mkRegex "(True|False)"
-integerRegex = mkRegex "([[:digit:]]+)"  -- start with nonnegative integers for now
-whitespaceRegex = mkRegex "([[:space:]]+)"
 
 mkRegex :: String -> Regex
 mkRegex s = mkRegexWithOpts ("^" ++ s) True True
 
-maybeToList :: Maybe a -> [a]
-maybeToList (Just a) = [a]
-maybeToList (Nothing) = []
+boolRe = mkRegex "(true|false)"
+intRe = mkRegex "([[:digit:]]+)"  -- start with nonnegative integers for now
 
 tokenizeLine :: String -> [Token]
-tokenizeLine line = case result of ((_,[]):(_,[]):(_,[]):_) -> []
-                                   ((_,[]):(_,[]):(x,y):_) -> tokenizeLine x
-                                   ((_,[]):(x,y):_) -> (processMatches IntegerToken y) ++ tokenizeLine x
-                                   ((x,y):_) -> (processMatches BoolToken y) ++ tokenizeLine x
-    where result = subtokenizeLine [boolRegex, integerRegex, whitespaceRegex] line
+tokenizeLine [] = []
+tokenizeLine line@(x:xs)
+    | x == ' ' = tokenizeLine xs
+    | otherwise = token:tokenizeLine remainder
+    where (token, remainder) = subtokenizeLine [(boolRe, TBool), (intRe, TInt)] line
 
-subtokenizeLine :: [Regex] -> String -> [(String, [String])]
-subtokenizeLine [] _ = []
-subtokenizeLine (r:rs) s = case result of [] -> ("",[]):subtokenizeLine rs s
-                                          [(_, _, rem, matches)] -> (rem, matches):[("",[]) | s' <- rs]
-    where result = maybeToList $ matchRegexAll r s
-
-processMatches :: (Read a) => (a -> Token) -> [String] -> [Token]
-processMatches t m = [t (read m') | m' <- m]
+subtokenizeLine :: [(Regex, (String -> Token))] -> String -> (Token, String)
+subtokenizeLine [] _ = (TErr, "")
+subtokenizeLine ((x,t):xs) s = case result of Nothing -> subtokenizeLine xs s
+                                              (Just(_, m, r, _)) -> (t m, r)
+    where result = matchRegexAll x s
