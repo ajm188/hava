d44edfe0f1a3740d3139cab1d6e2b61291695e59
Andrew Mason <mason@case.edu>
2015-10-17 00:23:06 -0400

    full parser (as much as I feel like adding now anyway)

--- Parse.hs
+++ Parse.hs
@@ -4,14 +4,37 @@ module Parse
 
 import Lex
 
+data Stmt = AssignStmt Expr BinaryOp Expr | ExprStmt Expr
+    deriving Show
+
 data Expr =
     Var String |
     Const Int |
     BinaryExpr Expr BinaryOp Expr
     deriving Show
 
-data BinaryOp = Add | Sub | Mul | Div | Mod
+data BinaryOp = Add | Sub | Mul | Div | Mod | Assign
     deriving Show
+
+parseStmts :: [[Token]] -> [Stmt]
+parseStmts [] = []
+parseStmts (x:xs) = (parseStmt x):(parseStmts xs)
+
+parseStmt :: [Token] -> Stmt
+parseStmt (x:[]) = ExprStmt $ parseExpr [x]
+parseStmt stmt@(x:t@(TOp o):xs)
+    | o == "+" || o == "-" = ExprStmt $ parseExpr stmt
+    | o == "*" || o == "/" || o == "%" = ExprStmt $ parseTerm stmt
+    | o == "=" = parseAssignment x xs
+
+parseAssignment :: Token -> [Token] -> Stmt
+parseAssignment x (x':[]) = AssignStmt (parseExpr [x]) Assign (parseExpr [x'])
+parseAssignment x rem@(x':(TOp o):xs) = AssignStmt (parseExpr [x]) Assign rhs
+    where rhs = case o of "+" -> parseExpr rem
+                          "-" -> parseExpr rem
+                          "*" -> parseTerm rem
+                          "/" -> parseTerm rem
+                          "%" -> parseTerm rem
 
 parseExpr :: [Token] -> Expr
 parseExpr ((TInt i):[]) = Const (read i)
@@ -30,3 +53,4 @@ binOp (TOp "-") = Sub
 binOp (TOp "*") = Mul
 binOp (TOp "/") = Div
 binOp (TOp "%") = Mod
+binOp (TOp "=") = Assign
